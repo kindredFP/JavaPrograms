d2c073c9338af63a71987c8fe530a50fd163b5d7
kindred_fp <kindredfp@users.noreply.github.com>
2020-01-22 22:41:01 -0500

    Added some selenium and http client methods

---
+++ TestNGProject/src/com/mypackage/francis/SeleniumNavigationTest.java
@@ -0,0 +1,73 @@
+package com.mypackage.francis;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SeleniumNavigationTest {
+
+    @Test
+    public void test() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "/Users/fpalattao/Documents/Dropbox/gitRepository/gitHubRepos/JavaPrograms/resourceFiles/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://demo.guru99.com/insurance/v1/index.php");
+
+        Thread.sleep(1000);  // Let the user actually see something!
+
+
+        WebElement button = driver.findElement(By.name("submit"));
+        System.out.println("before button click" + driver.getCurrentUrl());
+        System.out.println("driver.getWindowHandle" + driver.getWindowHandle());
+        System.out.println("driver.getWindowHandles" + driver.getWindowHandles());
+
+        button.click();
+        System.out.println("After button click" + driver.getCurrentUrl());
+        System.out.println("driver.getWindowHandle" + driver.getWindowHandle());
+        System.out.println("driver.getWindowHandles" + driver.getWindowHandles());
+
+        WebElement link = driver.findElement(By.id("ui-id-2"));
+        System.out.println("After link click" + driver.getCurrentUrl());
+        System.out.println("driver.getWindowHandle" + driver.getWindowHandle());
+        System.out.println("driver.getWindowHandles" + driver.getWindowHandles());
+
+        Thread.sleep(1000);  // Let the user actually see something!
+        driver.quit();
+    }
+
+    @Test
+    public void navigationWindowTest() throws Exception {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/fpalattao/Documents/Dropbox/gitRepository/gitHubRepos/JavaPrograms/resourceFiles/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://demo.guru99.com/payment-gateway/index.php");
+
+        Thread.sleep(1000);  // Let the user actually see something!
+
+
+        WebElement button = driver.findElement(By.linkText("Generate Card Number"));
+        System.out.println("before button click" + driver.getCurrentUrl());
+        System.out.println("driver.getWindowHandle" + driver.getWindowHandle());
+        System.out.println("driver.getWindowHandles" + driver.getWindowHandles());
+
+        button.click();
+        System.out.println("After button click" + driver.getCurrentUrl());
+        System.out.println("driver.getWindowHandle" + driver.getWindowHandle());
+        System.out.println("driver.getWindowHandles" + driver.getWindowHandles());
+
+        for (String myCurrentHandle : driver.getWindowHandles()) {
+
+            driver.switchTo().window(myCurrentHandle);
+            System.out.println("driver.getWindowHandle" + driver.getWindowHandle());
+
+        }
+        driver.switchTo().defaultContent();
+
+        Thread.sleep(1000);  // Let the user actually see something!
+        driver.quit();
+    }
+}
